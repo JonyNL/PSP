5eede4a1d0530b7d0a0d8b8b1476c7aa2ca8fb14
Jonathan Nemi Lato <37952437+JonyNL@users.noreply.github.com>
2019-02-21 16:05:33 +0100

    Create Server.java

---
+++ Java/Sockets/Server.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        // 1.- Creamos un socket esperando clientes
+        ServerSocket serverSocket = new ServerSocket(9090);
+        System.out.println("esperando clientes...");
+
+        // cliente intenta conectar con el servidor
+        Socket socket = serverSocket.accept();
+
+        // 2.- OUT -> Cliente
+        // Necesitamos un stream para enviar datos al cliente
+        // Vamos a enviar un mensaje al cliente con PrintWriter
+        PrintWriter out  = new PrintWriter(socket.getOutputStream(), true);
+        out.println("Hola cliente");
+
+        // 3.- IN <- Cliente
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String clientInput = input.readLine();
+        System.out.println(clientInput);
+
+        //4.- Cerrar stream socket
+        input.close();
+        out.close();
+        socket.close();
+        serverSocket.close();
+    }
+}
